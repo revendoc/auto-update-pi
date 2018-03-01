857bfda64c7c388c0ee74fe422738dea22f0e2af
sregiT <jegry.dev@gmail.com>
2018-03-01 01:48:22 -0500

    version 1.0

--- update_now.sh
+++ update_now.sh
@@ -1,6 +1,13 @@
-!#/bin/bash
+#!/bin/bash
+######################
+# auto-update-pi
+# description: script to automatically 
+#  install updates
+######################
 
-apt-get update
-apt-get upgrade
+apt-get --quiet --assume-yes update
+
+apt-get -quiet --assume-yes upgrade
+
 reboot
 
